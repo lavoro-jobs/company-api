33602ef9d36626d800d934a5d75cf1eacf264345
Marko Bolt <marko.bolt@fer.hr>
2023-12-06 23:31:32 +0100

    Changed models organization

--- lavoro_company_api/routers/company.py
+++ lavoro_company_api/routers/company.py
@@ -8,14 +8,15 @@ from lavoro_company_api.database.queries import delete_invitation, get_company_b
 from lavoro_company_api.helpers.company_helpers import create_company_and_update_recruiter, validate_company_creation
 from lavoro_company_api.helpers.email_helpers import send_invite_email
 from lavoro_company_api.helpers.invitation_helpers import create_invite_token
-from lavoro_library.models import CreateCompanyRequest
 
+# from lavoro_library.models import CreateCompanyRequest
+from lavoro_library.model.company_api.dtos import CreateCompanyDTO
 
 router = APIRouter(prefix="/company", tags=["company"])
 
 
 @router.post("/create-company/{recruiter_account_id}")
-def create_company(payload: CreateCompanyRequest, recruiter_account_id: uuid.UUID):
+def create_company(payload: CreateCompanyDTO, recruiter_account_id: uuid.UUID):
     validate_company_creation(recruiter_account_id)
     return create_company_and_update_recruiter(payload, recruiter_account_id)
 
